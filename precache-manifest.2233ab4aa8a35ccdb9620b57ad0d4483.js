8ef0c48b43cc0247b3d4c16652bee93ce94de96e
Rafael <rafa.arraez.gue@gmail.com>
2020-08-24 23:09:46 -0400

    Updates

---
+++ precache-manifest.2233ab4aa8a35ccdb9620b57ad0d4483.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "195d01f60c7c967614a168939accb93d",
+    "url": "/futboll-team-app/index.html"
+  },
+  {
+    "revision": "c1f0cc1e59bb6678a35b",
+    "url": "/futboll-team-app/static/css/main.c060ea37.chunk.css"
+  },
+  {
+    "revision": "770156de973789ecac67",
+    "url": "/futboll-team-app/static/js/2.913be618.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/futboll-team-app/static/js/2.913be618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1f0cc1e59bb6678a35b",
+    "url": "/futboll-team-app/static/js/main.976b8076.chunk.js"
+  },
+  {
+    "revision": "bd24f939a1de9477bd0a",
+    "url": "/futboll-team-app/static/js/runtime-main.d96e29ad.js"
+  },
+  {
+    "revision": "aa6d54bfa22c49cf58b85efd6f82d010",
+    "url": "/futboll-team-app/static/media/cancha.aa6d54bf.svg"
+  }
+]);
